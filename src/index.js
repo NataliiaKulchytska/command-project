57be61df0c1bb35cc17edc19bd209ee8b59809fe
NataliiaKulchytska <kylchitskaia@gmail.com>
2022-06-21 13:59:03 +0300

    copy

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './js/scroll';
+import './js/about-modal';
+import './js/reviews-slider';
+import './js/gallery-viever';
+import './js/menu';
+import './js/modal';
+import './js/modal-contacts';
+import './js/hero-overlay';
